f6544e60b29cda93db515cd56f2489e3a1cebf47
camilo <juancamiloruiz@unicomfacauca.edu.co>
2026-03-30 13:34:34 -0500

    Trabajo deploy ML

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://trabajoml.onrender.com"],  
-    allow_credentials=True,
+    allow_origins=["*"],  
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
